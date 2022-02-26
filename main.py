7c8365c187299e5c88b8e092bc9789631d816fdc
TheNha <thenha1107@gmail.com>
2022-02-26 15:09:14 +0700

    test edit 8

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'Auto Deploy by Devops!'
+    return 'Edit update deploy!'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
